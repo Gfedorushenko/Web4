0d3981f015f23a1bb275a1750543545018e13b70
Григорий Федорущенко <Gfedorushenko@mail.ru>
2023-01-14 19:36:30 +0300

    first commit

---
+++ src/main/java/ru/netology/controller/PostController.java
@@ -0,0 +1,56 @@
+package ru.netology.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private static final Gson gson = new Gson();
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        final var data = service.all();
+        System.out.println(data);
+        Type listType = new TypeToken<List<Post>>() {
+        }.getType();
+        response.setContentType(APPLICATION_JSON);
+        response.getWriter().print(gson.toJson(data, listType));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data=service.getById(id);
+        if(data.isEmpty()) {
+            response.setStatus(404);
+            response.getWriter().print("id: " + id + " not found");
+        }
+        else
+            response.getWriter().print(gson.toJson(data));
+
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        response.setContentType(APPLICATION_JSON);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        response.getWriter().print("id: " + id + " removed");
+    }
+}
---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,32 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class PostRepository {
+
+  private static final ConcurrentSkipListMap<Long, Post> requests = new ConcurrentSkipListMap<>();
+  public List<Post> all() {
+    List<Post> list = new ArrayList<>();
+    for(Post post : requests.values()){
+      list.add(post);
+    }
+    return list;
+  }
+
+  public Optional<Post> getById(long id) {return Optional.ofNullable(requests.get(id));}
+
+  public Post save(Post post) {
+    long count=post.getId();
+    if(count==0)
+      count=requests.lastKey()+1;
+    post.setId(count);
+    requests.put(count, post);
+    return post;
+  }
+  public void removeById(long id) { requests.remove(id);  }
+}
